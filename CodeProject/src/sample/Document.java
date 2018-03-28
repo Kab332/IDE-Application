d2b8994b873b6cb17aabff96addbf7b581f26f38
ThomasLaptopWindows <thomas.chapman@uoit.net>
2018-03-28 13:41:52 -0400

    Added functionality to Document class

--- CodeProject/src/sample/Document.java
+++ CodeProject/src/sample/Document.java
@@ -1,9 +1,19 @@
 package sample;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Document {
+
+    public String sinceLast;
+    /**
+     * sinceLast:
+     *      r:5 removed 5
+     *      a:9 added 9
+     *      e:3 edit on line 3
+     */
+
 
     public List<Line> myLines;
     public int numOfLines = -1;
@@ -18,9 +28,13 @@ public class Document {
      * @param index     index to remove
      */
     public void removeLine(int index){
-        myLines.remove(index);
-        numOfLines-=1;
+        if(numOfLines >= index) {
+            myLines.remove(index);
+            numOfLines -= 1;
+            sinceLast+=";r:"+index;
+        }
     }
+
 
     /**
      * Adds a line at a specific index
@@ -29,16 +43,22 @@ public class Document {
      * @param toBeAdded     string to be added
      */
     public void addLine(int index, String toBeAdded){
+
         if(numOfLines < index){
             myLines.add(new Line(toBeAdded));
         }else{
             myLines.add(index, new Line(toBeAdded));
         }
-
         numOfLines+=1;
+        sinceLast+=";a:"+index;
     }
 
+    public void changed(String inBundle) {
+        List<String> newBundle = new ArrayList<>(Arrays.asList(inBundle.split("\n")));
 
+
+
+    }
 
 
     public String toString(){
