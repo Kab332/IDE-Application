627510f4ac1dd4d1d84dc174d58f06f605b1b29e
Kab332 <kab_ilan@hotmail.com>
2018-03-28 18:44:48 -0400

    Modified Server class to handle clients leaving while the server is running

--- CodeProject/src/sample/Server.java
+++ CodeProject/src/sample/Server.java
@@ -1,13 +1,21 @@
 package sample;
 
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Server extends Thread {
     ServerSocket serverSocket;
     ClientConnectionHandler [] threads;
-    int server_port;
 
+    int server_port;
     int number_of_clients = 0;
+
+
     int max_clients = 999;
 
     public Server() {
@@ -20,8 +28,6 @@ public class Server extends Thread {
     }
 
     public void run() {
-        String message = "";
-
         try {
             serverSocket = new ServerSocket(server_port);
             serverSocket.setSoTimeout(0);
@@ -29,14 +35,28 @@ public class Server extends Thread {
             threads = new ClientConnectionHandler[max_clients];
 
             while (true) {
-                threads[number_of_clients] = new ClientConnectionHandler(serverSocket.accept());
+                threads[number_of_clients] = new ClientConnectionHandler(serverSocket.accept(), number_of_clients+1);
                 threads[number_of_clients].start();
-                System.out.println("Client connection received.");
 
+                System.out.println("Client #" + (number_of_clients + 1) + " has connected");
                 number_of_clients++;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public void sendAll(String message){
+        try {
+            for (int i = 0; i < number_of_clients; i++) {
+                if (threads[i].isOpen) {
+                    System.out.println("Sent to Client #" + threads[i].clientNumber);
+                    threads[i].sendMessage(message);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
