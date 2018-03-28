43f516c5bc59280f43baa31609a5d6caa9915665
ThomasLaptopWindows <thomas.chapman@uoit.net>
2018-03-28 13:15:42 -0400

    Added functionality to Document and Line classes

--- CodeProject/src/sample/Document.java
+++ CodeProject/src/sample/Document.java
@@ -1,17 +1,56 @@
 package sample;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Document {
 
     public List<Line> myLines;
+    public int numOfLines = -1;
 
+    public Document(){
+        myLines = new ArrayList<>();
+    }
+
+    /**
+     * removes a line at a certain index
+     *
+     * @param index     index to remove
+     */
     public void removeLine(int index){
         myLines.remove(index);
+        numOfLines-=1;
     }
 
+    /**
+     * Adds a line at a specific index
+     *
+     * @param index         index where line will be added
+     * @param toBeAdded     string to be added
+     */
     public void addLine(int index, String toBeAdded){
-        myLines.add(index, new Line(toBeAdded));
+        if(numOfLines < index){
+            myLines.add(new Line(toBeAdded));
+        }else{
+            myLines.add(index, new Line(toBeAdded));
+        }
+
+        numOfLines+=1;
     }
+
+
+
+
+    public String toString(){
+        String ret = "";
+
+        for(Line i : myLines){
+            ret = ret + i.toString();
+        }
+
+        return ret;
+    }
+
+
 
 }
--- CodeProject/src/sample/Line.java
+++ CodeProject/src/sample/Line.java
@@ -4,7 +4,16 @@ public class Line {
     public int lineNumber;
     public boolean hasChanged;
     public String line;
+
     public Line(String line){
         this.line = line;
+    }
+
+    public boolean isDifferent(String toCheck){
+        return toCheck.equals(line);
+    }
+
+    public String toString(){
+        return line+"\n";
     }
 }
