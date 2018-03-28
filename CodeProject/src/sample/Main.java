613d4d09a94102fc25257f1627846c9f3bce2232
musabbirbaki <musabbir.a.b@hotmail.com>
2018-03-28 18:27:27 -0400

    Updating sample.fxml

--- CodeProject/src/sample/Main.java
+++ CodeProject/src/sample/Main.java
@@ -10,10 +10,19 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = (Parent) loader.load();
+        //Controller controller = (Controller) loader.getController();
+        Scene scene = new Scene(root, 759, 600);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
+
+
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root, 600, 500));
+//        primaryStage.show();
     }
 
 
